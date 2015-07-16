2f109e5fb63f5525bad7521ff280e32b56486383
Hannah Krager <hannahkrager@gmail.com>
2015-07-16 13:48:22 -0700

    reformat template to work on heroku

--- project_robot/bots/views.py
+++ project_robot/bots/views.py
@@ -8,7 +8,6 @@ from .models import Robot
 
 class HomePage(ListView):
     template_name = 'bots/index.html'
-    context_object_name = 'robot_report'
 
     def get_queryset(self):
         return Robot.objects.order_by('name')
