fbce2aaa0252aed85b70e37dd63136a9d08f9784
Hannah Krager <hannahkrager@gmail.com>
2015-07-17 08:02:04 -0700

    reconfig databases

--- project_robot/project_robot/settings.py
+++ project_robot/project_robot/settings.py
@@ -77,14 +77,13 @@ WSGI_APPLICATION = 'project_robot.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
-# Parse database configuration from $DATABASE_URL
-import dj_database_url
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default='sqlite3:////{0}'.format(os.path.join(BASE_DIR, 'db.sqlite3')))
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
-
 
 
 # Internationalization
